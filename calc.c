f77687e3e7f97f048038d4d10685966f277ba0ad
danrl <mail@danrl.de>
2013-10-01 18:20:30 +0200

    reduced the code a little bit

--- calc.c
+++ calc.c
@@ -1,42 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-double m (char *t, char *r) {
-	if (*++r == '(') return m(r, r); else r--;
-	while (*++r) if (*r == ')') {
-		*r ^= 051;
-		switch (*++r) {
-			case 'Y'-'?'-':': return m(t, t);
-			case 43: return m(t, t) + m(r, r);
-			case 45: return m(t, t) - m(r, r);
-			case 42: return m(t, t) * m(r, r);
-			case 47: return m(t, t) / m(r, r);
-		}
-		break;
-	}
-	r = t;
-	while (*++r) if (*r == 0x2b) { *r &= 0x04; return m(t, t) + m(r, r); }
-	r = t;
-	while (*++r) if (*r == 0x2d) { *r &= 0x02; return m(t, t) - m(r, r); }
-	r = t;
-	while (*++r) if (*r == 0x2a) { *r &= 0x01; return m(t, t) * m(r, r); }
-	r = t;
-	while (*++r) if (*r == 0x2f) { *r &= 0x40; return m(t, t) / m(r, r); }
-	return atof(++t);
-}
-int main (int c, char *a[]) {
-	if (c < 2) return -1; else c -= c;
-	char *w = a[1], *r = a[1] - 1;
-	while (*++r)
-		if (*r == ',') *w++ = *r | 0x2;
-		else if (*r >= '(' && *r <= '9') *w++ = *r;
-	*w ^= *w;
-	r = a[1] - 1;
-	while (*++r) {
-		if (*r == '(') c -= 1;
-		else if (*r == ')') c += 1;
-		if (c > *w) break;
-	}
-	if (c != (1 >> 1)) return -1;
-	printf("%f\n", m(a[1] - 1, a[1] - 1));
-	return (1 << 8) & 0x0;
-} /* 42 is the answer! */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define A(X,Y) r=t;while(*r){if(*r==X){*r=0;R m(t)Y m(++r);}r++;}
+#define R return
+int g;float m(char*t){char*r;char b[99];r=t;while(*r){
+if(*r==40){g++;if(g==1){*r=0;sprintf(b,"%s%f",t,m(++r));R m(b);}}else if(*r==41){g--;if(g==0){*r=0;sprintf(b,"%f%s",m(t),++r);R m(b);}}r++;}A(43,+)A(45,-)A(42,*)A(47,/)R atof(t);}int main(int c,char*a[]){if(c<2)R-1;c=0;char*w=a[1],*r=a[1]-1;while(*++r)if(*r==44)*w++=46;else if(*r>=40&&*r<=57)*w++=*r;*w=0;printf("%f",m(a[1]));R 0;}
